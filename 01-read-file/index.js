34d1c89e1ef8c01e5b571e588d190031fad8f0b6
fronkin <feko992@gmail.com>
2023-05-09 18:17:14 +0300

    fix:  task №1

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,4 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readFile('text.txt', 'utf8', function(error, fileContent){
-    if(error) throw error; // ошибка чтения файла, если есть
-    console.log(fileContent); // содержимое файла
-});
+fs.ReadStream(path.join(__dirname, 'text.txt')).pipe(process.stdout);
